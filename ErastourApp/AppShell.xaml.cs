46df9c40ebb92a73912a3cba6f40e96c01fdaa3e
Timur <nftgameur989@gmail.com>
2026-03-04 14:16:54 +0100

    Fix gitignore

---
+++ ErastourApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ErastourApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ErastourApp/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "ErastourApp")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "ErastourApp.Pages")]
