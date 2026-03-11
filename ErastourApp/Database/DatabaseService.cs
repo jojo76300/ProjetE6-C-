e05c14d02ccfadb73b125d2d03f5b0c3fa441faf
Jenzoov <enzovct22@gmail.com>
2026-03-11 11:35:43 +0100

    Début et ajout du DatabaseService

---
+++ ErastourApp/Database/DatabaseService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace ErastourApp.Database
+{
+    public partial class DatabaseService
+    {
+        private readonly SQLiteConnection _database;
+        public const string DatabaseFileName = "erastour.db";
+        public const SQLiteOpenFlags Flags = 
+        SQLiteOpenFlags.ReadWrite |
+        SQLiteOpenFlags.Create |
+        SQLiteOpenFlags.SharedCache;
+
+        public DatabaseService()
+        {
+
+        }
+    }
+}
