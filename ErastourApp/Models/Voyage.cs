80837983f2e2bbaabcb96eb46751e316164ee631
Jenzoov <enzovct22@gmail.com>
2026-03-18 11:04:05 +0100

    ajout du model voyage

---
+++ ErastourApp/Models/Voyage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace ErastourApp.Models
+{
+    public class Voyage
+    {
+        [PrimaryKey, AutoIncrement] public int Voyage_Id { get; set; }
+    }
+}
