9cf797668ea3d64acf9d1b81d26d508cb35850e4
Timur <nftgameur989@gmail.com>
2026-03-18 16:27:25 +0100

    mes modifs

---
+++ ErastourApp/Pages/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using ErastourApp.ViewModels;
+namespace ErastourApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel();
+        }
+    }
+}
---
+++ ErastourApp/ViewModels/LoginViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ErastourApp.ViewModels;
+
+public class LoginViewModel
+{
+    public string Login { get; set; }
+    public string Password { get; set; }
+
+    public ICommand LoginCommand { get; }
+
+    public LoginViewModel()
+    {
+        LoginCommand = new Command(OnLogin);
+    }
+
+    private async void OnLogin()
+    {
+        if (Login == "admin" && Password == "1234")
+        {
+            await Shell.Current.DisplayAlert("Succès", "Connexion OK", "OK");
+        }
+        else
+        {
+            await Shell.Current.DisplayAlert("Erreur", "Identifiants incorrects", "OK");
+        }
+    }
+}
