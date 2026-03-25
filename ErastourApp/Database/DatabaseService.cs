2386ad2ba6e4825eeac6d6d5d3681bdf81f6e692
Jenzoov <enzovct22@gmail.com>
2026-03-25 11:59:35 +0100

    databaseservice V1

--- ErastourApp/Database/DatabaseService.cs
+++ ErastourApp/Database/DatabaseService.cs
@@ -1,23 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using SQLite;
+using Microsoft.Data.SqlClient;
+using ErastourApp.Models;
+
 
 namespace ErastourApp.Database;
 
 public partial class DatabaseService
 {
-    private readonly SQLiteConnection _database;
-    public const string DatabaseFileName = "erastour.db";
-    public const SQLiteOpenFlags Flags = 
-    SQLiteOpenFlags.ReadWrite |
-    SQLiteOpenFlags.Create |
-    SQLiteOpenFlags.SharedCache;
+    private SqlConnection Connexion { get; set; }
 
     public DatabaseService()
     {
+        var connectionStringBuilder = new SqlConnectionStringBuilder
+        {
+            DataSource = "localhost",
+            InitialCatalog = "Erastour",
+            UserID = "sa",
+            Password = "Info76240#",
+            TrustServerCertificate = true,
+        };
 
+        //Création de la connexion à la base de données avec la chaîne de connexion
+        Connexion = new SqlConnection(connectionStringBuilder.ConnectionString);
+        Connexion.Open();
+    }
+
+    public List<Lieu> GetLieux()        
+    {
+        string queryString = "GetLieux";
+        SqlCommand command = new SqlCommand(queryString, Connexion);
+        command.CommandType = System.Data.CommandType.StoredProcedure;
+        SqlDataReader reader = command.ExecuteReader();
+        var lieux = new List<Lieu>();
+        while (reader.Read())
+        {
+            lieux.Add(
+                new Lieu
+                {
+                    Lieu_Id = (int)reader["Lieu_Id"],
+                    Lieu_Nom = (string?)reader["Lieu_Nom"],
+                    Lieu_Ville = (string?)reader["Lieu_Ville"],
+                    Lieu_Pays = (string?)reader["Lieu_Pays"],
+                    Lieu_CP = (string?)reader["Lieu_CP"],
+                    Lieu_Adresse = (string?)reader["Lieu_Adresse"],
+                    Lieu_LienMap = (string?)reader["Lieu_LienMap"],
+                    Lieu_Commentaire = (string?)reader["Lieu_Commentaire"] 
+                });
+        }
+        return lieux;
     }
 }
